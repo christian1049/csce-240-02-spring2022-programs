f3a226d3d37035e5647bb3b68a6a57cd15113964
Christian Lee <christianlee1049@gmail.com>
2022-03-02 22:31:55 -0500

    pa3

--- project/projectAssignments/prog1-extractor/src/project.java
+++ project/projectAssignments/prog1-extractor/src/project.java
@@ -66,7 +66,8 @@ public class project {
 
     public static void collectData(ArrayList<String> information) {
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\chris\\OneDrive\\Desktop\\CSCE240\\csce-240-02-spring2022-programs\\project\\projectAssignments\\prog1-extractor\\data\\output.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(
+                "C:\\Users\\chris\\OneDrive\\Desktop\\CSCE240\\csce-240-02-spring2022-programs\\project\\projectAssignments\\prog1-extractor\\data\\output.txt"))) {
             while (reader.ready()) {
                 information.add(reader.readLine());
             }
@@ -78,59 +79,124 @@ public class project {
 
     public static void populate(ArrayList<String> information) {
         boolean cont = true;
-        String name = information.get(0);
-        String region = information.get(2);
-        String addressCola = information.get(3);
-        String addressHome = information.get(4);
-        String phoneNumberBiz = information.get(5);
-        String phoneNumberHome = information.get(6);
-        String personalInfo = information.get(10);
+        String name = information.get(0) + "\n";
+        String region = information.get(2) + "\n";
+        String addressCola = information.get(3) + "\n";
+        String addressHome = information.get(4) + "\n";
+        String phoneNumberBiz = information.get(5) + "\n";
+        String phoneNumberHome = information.get(6) + "\n";
+        String personalInfo = "";
         for (int i = 0; i < 6; i++) {
-
             personalInfo += "\n" + information.get(i + 10);
         }
-        String committeeAssignments = information.get(17);
-        String bills = information.get(7);
-        String votingRec = information.get(7);
-        String servicePubOffice = information.get(7);
-
+        String committeeAssignments = information.get(17) + "\n";
+        String bills = information.get(7) + "\n";
+        String votingRec = information.get(7) + "\n";
+        String servicePubOffice = information.get(7) + "\n";
+        String contact = "\nContact Information: \n";
+        contact = contact + name + region + addressCola + addressHome + phoneNumberBiz + phoneNumberHome;
+        String all = name+region+addressCola+addressHome+phoneNumberBiz+phoneNumberHome+personalInfo+bills+servicePubOffice;
         Scanner keyboard = new Scanner(System.in);
+        
         while (cont) {
             System.out.println("What would you like to know about your representative?");
             String response = keyboard.nextLine();
-            if (response.toLowerCase().contains("name")) {
-                System.out.println(name);
+            if (response.toLowerCase().contains("name") || response.toLowerCase().contains("live")) {
+                getName(name);
             } else if (response.toLowerCase().contains("region")) {
-                System.out.println(region);
-            } else if (response.toLowerCase().contains("work address")
-                    || response.toLowerCase().contains("columbia address")) {
-                System.out.println(addressCola);
-            } else if (response.toLowerCase().contains("home address")) {
-                System.out.println(addressHome);
-            } else if (response.toLowerCase().contains("work phone number")) {
-                System.out.println(phoneNumberBiz);
-            } else if (response.toLowerCase().contains("home phone number")) {
-                System.out.println(phoneNumberHome);
-            } else if (response.toLowerCase().contains("personal information")) {
-                System.out.println(personalInfo);
-            } else if (response.toLowerCase().contains("committee assignments")) {
-                System.out.println(committeeAssignments);
+                getRegion(region);
+                contact.concat(name).concat(region).concat(addressCola).concat(addressHome).concat(phoneNumberBiz)
+                        .concat(phoneNumberBiz);
+            } else if (response.toLowerCase().contains("columbia")
+                    || response.toLowerCase().contains("work") && response.toLowerCase().contains("address")) {
+                getWorkAddress(addressCola);
+            } else if (response.toLowerCase().contains("home") && response.toLowerCase().contains("address")) {
+                getHomeAddress(addressHome);
+            } else if (response.toLowerCase().contains("work") && (response.toLowerCase().contains("phone")
+                    || response.toLowerCase().contains("number")) || response.toLowerCase().contains("work phone number")) {
+                getWorkPhone(phoneNumberBiz);
+            } else if (response.toLowerCase().contains("home") && response.toLowerCase().contains("phone")
+                    || response.toLowerCase().contains("number") || response.toLowerCase().contains("home phone number")) {
+                getHomePhone(phoneNumberHome);
+            } else if (response.toLowerCase().contains("personal") || response.toLowerCase().contains("information")) {
+                getPersonalInfo(personalInfo);
+            } else if (response.toLowerCase().contains("committee")) {
+                getCommitteeAssign(committeeAssignments);
             } else if (response.toLowerCase().contains("bills")) {
-                System.out.println("View this link in your browser to see " + bills);
-            } else if (response.toLowerCase().contains("voting record")) {
-                System.out.println("View this link in your browser to see " + votingRec);
-            } else if (response.toLowerCase().contains("service public office")) {
-                System.out.println("House of Representatives, 2017 - Present");
+                getBills(bills);
+            } else if (response.toLowerCase().contains("voting record") || response.toLowerCase().contains("vote")
+                    || response.toLowerCase().contains("record")) {
+                getRecord(votingRec);
+            } else if (response.toLowerCase().contains("service")
+                    || response.toLowerCase().contains("public") && response.toLowerCase().contains("office")) {
+                getServicePublic();
+            } else if (response.toLowerCase().contains("tell me about the rep")
+                    || response.toLowerCase().contains("contact")) {
+                getContact(contact);
+            } else if (response.toLowerCase().contains("everything") || response.toLowerCase().contains("all")) {
+                getAll(all);
             } else {
                 System.out.println("Try rewording phrase");
             }
             System.out.println("Would you like to ask another question?");
-            String another = keyboard.next();
-            if (another.equals("no")) {
+            String another = keyboard.nextLine();
+            if (another.contains("no") || another.toLowerCase().contains("quit") || another.toLowerCase().equals("q")
+                    || another.toLowerCase().equals("n")) {
                 System.out.println("Goodbye");
                 System.exit(0);
             }
         }
+    }
 
+    public static void getName(String name) {
+        System.out.println("\n" + name);
+    }
+
+    public static void getRegion(String region) {
+        System.out.println("\nRegion: " + region);
+    }
+
+    public static void getWorkAddress(String workAdd) {
+        System.out.println("\nWork Address: " + workAdd);
+    }
+
+    public static void getHomeAddress(String homeAdd) {
+        System.out.println("\nHome Address: " + homeAdd);
+    }
+
+    public static void getWorkPhone(String workPhone) {
+        System.out.println("\nWork Phone Number: " + workPhone);
+    }
+
+    public static void getHomePhone(String homePhone) {
+        System.out.println("\nHome Phone Number: " + homePhone);
+    }
+
+    public static void getPersonalInfo(String personalInfo) {
+        System.out.println("\nPersonal Information:\n" + personalInfo + "\n");
+    }
+
+    public static void getCommitteeAssign(String committeeAssign) {
+        System.out.println("\nCommittee Assignments: " + committeeAssign);
+    }
+
+    public static void getBills(String bills) {
+        System.out.println("\nView this link in your browser to see bills " + bills);
+    }
+
+    public static void getRecord(String record) {
+        System.out.println("\nView this link in your browser to see voting record " + record);
+    }
+
+    public static void getServicePublic() {
+        System.out.println("\nHouse of Representatives, 2017 - Present\n");
+    }
+
+    public static void getAll(String all) {
+        System.out.println(all);
+    }
+
+    public static void getContact(String contact) {
+        System.out.println(contact);
     }
 }
